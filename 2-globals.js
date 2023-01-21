6fefbb2a0773b465542c55bca3c6ec7242a1d833
yushanwebdev <yushanwebdev@gmail.com>
2023-01-21 17:32:40 +0530

    feat: Globals

---
+++ 2-globals.js
@@ -0,0 +1,16 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+// console
+// setTimeout
+// setInterval
+
+console.log("__dirname", __dirname);
+
+setInterval(() => {
+  console.log("hello");
+}, 1000);
